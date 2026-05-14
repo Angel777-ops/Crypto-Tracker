4f8d6985442099cbd4c5d762b62d8c562ba10a48
Angel7key <angel77key@hotmail.com>
2026-05-13 19:54:39 -0600

    fix: correccion definitivo del grafico tendencial en CoinTrendCharts con un estado de control de ciclo de vida

--- src/components/CoinTrendChart.js
+++ src/components/CoinTrendChart.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { ChartWrapper } from '../pages/Details/styles';
 
 const CoinTrendChart = ({ chartData, isUp }) => {
+  // --- Estado de Control de Ciclo de Vida ---
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <>
       <h3>Tendencia últimos 7 días</h3>
       <ChartWrapper>
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart data={chartData}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="date" hide /> 
-            <YAxis domain={['auto', 'auto']} hide />
-            <Tooltip />
-            <Line 
-              type="monotone" 
-              dataKey="price" 
-              stroke={isUp ? "#4caf50" : "#f44336"} 
-              strokeWidth={3} 
-              dot={false} 
-            />
-          </LineChart>
-        </ResponsiveContainer>
+        
+        {isMounted && (
+          <ResponsiveContainer width="100%" height={300} minHeight={300}>
+            <LineChart data={chartData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" hide /> 
+              <YAxis domain={['auto', 'auto']} hide />
+              <Tooltip />
+              <Line 
+                type="monotone" 
+                dataKey="price" 
+                stroke={isUp ? "#4caf50" : "#f44336"} 
+                strokeWidth={3} 
+                dot={false} 
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </ChartWrapper>
     </>
   );
